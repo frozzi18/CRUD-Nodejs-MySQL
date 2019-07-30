270cd6ef9b003a1664efb860463d2ef7836864b8
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-30 11:21:24 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql');
+const express = require('express');
+var app = express();
+
+const bodyparser = require('body-parser')
+
+app.use(bodyparser.json());
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'employeedb'
+})
+
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('DB connection succeded.');
+    else
+        console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+
+}) 
+
+app.listen(3000, ()=>console.log('Express server running at port no : 3000'))
+
+app.get('/employees',(res,req)=>{
+    mysqlConnection.query('SELECT * FROM Employee', (err, rows, fields)=>{
+        if(!err)
+        console.log(rows);
+        else
+        console.log(err);
+    })
+})
