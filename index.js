ea4dc5eba8616189f8fa31dfcd222f6c59cc0afa
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-07-30 11:30:23 +0200

    update

--- index.js
+++ index.js
@@ -23,10 +23,11 @@ mysqlConnection.connect((err) => {
 
 app.listen(3000, ()=>console.log('Express server running at port no : 3000'))
 
-app.get('/employees',(res,req)=>{
+//Get all employees
+app.get('/employees',(req,res)=>{
     mysqlConnection.query('SELECT * FROM Employee', (err, rows, fields)=>{
         if(!err)
-        console.log(rows);
+        res.send(rows)
         else
         console.log(err);
     })
